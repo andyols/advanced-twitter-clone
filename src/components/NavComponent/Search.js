e3c90b7288fcf42c41883a76d8c5df6a0b7db11c
andyols <andyolson09@gmail.com>
2019-03-01 17:22:34 -0600

    navbar css

--- src/components/NavComponent/Search.js
+++ src/components/NavComponent/Search.js
@@ -3,13 +3,22 @@ import React from 'react'
 const Search = () => {
   return (
     <ul className="right">
-      <li>
+      <li className="search">
         <form>
           <div className="input-field">
-            <i className="fas fa-search prefix" />
-            <input placeholder="Search Twitter" id="search" type="text" />
+            <input id="search" type="text" placeholder="Search Twitter" />
           </div>
         </form>
+      </li>
+      <li>
+        <a className="waves-effect waves-light btn btn-floating grey darken-1">
+          AO
+        </a>
+      </li>
+      <li>
+        <a className="waves-effect waves-light btn blue lighten-1 btn-tweet">
+          Tweet
+        </a>
       </li>
     </ul>
   )
