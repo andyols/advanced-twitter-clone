6a246a53e5b885142688699e3370c56c718a8386
andyols <andyolson09@gmail.com>
2019-03-01 11:23:54 -0600

    nav links

--- src/components/NavComponent/index.js
+++ src/components/NavComponent/index.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import './navComponent.css'
 
 const NavComponent = () => {
   return (
     <nav>
       <div className="nav-wrapper grey lighten-3">
-        <a href="#" className="brand-logo center">
-          Logo
+        <a href="#" className="brand-logo center blue-text text-lighten-1">
+          <i className="fab fa-twitter" />
         </a>
-        <ul id="nav-mobile" className="left hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Sass</a>
+        <ul id="nav-mobile" className="left">
+          <li className="active">
+            <a href="#" className="grey-text text-darken-2">
+              <i className="fas fa-home left" />
+              <span className="nav-link">Home</span>
+            </a>
           </li>
           <li>
-            <a href="badges.html">Components</a>
+            <a href="#" className="grey-text text-darken-2">
+              <i className="fas fa-bolt left" />
+              <span className="nav-link">Moments</span>
+            </a>
           </li>
           <li>
-            <a href="collapsible.html">JavaScript</a>
+            <a href="#" className="grey-text text-darken-2">
+              <i className="fas fa-bell left" />
+              <span className="nav-link">Notifications</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="grey-text text-darken-2">
+              <i className="fas fa-envelope left" />
+              <span className="nav-link">Messages</span>
+            </a>
           </li>
         </ul>
       </div>
