112453b294751de92c19595a8351357b75b10ab7
Andy Olson <andyolson09@gmail.com>
2019-02-28 20:07:10 -0600

    nav component setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+import NavComponent from './components/NavComponent/index';
 import LeftComponent from './components/LeftComponent/index';
 import MiddletComponent from './components/MiddleComponent/index';
 import RightComponent from './components/RightComponent/index';
 
 function App() {
   return (
-    <div className="App">
-      <LeftComponent />
-      <MiddletComponent />
-      <RightComponent />
+    <div>
+      <NavComponent />
+      <div className="App">
+        <LeftComponent />
+        <MiddletComponent />
+        <RightComponent />
+      </div>
     </div>
   );
 }
---
+++ src/components/NavComponent/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NavComponent.css';
+
+const NavComponent = () => {
+  return <div className="nav" />;
+};
+
+export default NavComponent;
